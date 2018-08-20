a8920f15809b2424f5287f9e2858b636fa13d2a3
Lodwin Moloto <33810308+Sebolaishi@users.noreply.github.com>
2018-08-20 09:33:05 -0700

    Add files via upload

---
+++ python sorted list.py
@@ -0,0 +1,13 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+numbers = [5, 2, 1, 4, 3]
+print(sorted(numbers, key=int))
+
+names = ["Mahlatse", "Academy","Digital"]
+print(sorted(names, key=str))
